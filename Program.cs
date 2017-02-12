511e3ac241f90b5b3a2dd4debfbea96c570e5dde
jkhanseong <jkhanseong@users.noreply.github.com>
2017-02-12 11:13:32 +0900

    GitHub for git pull

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace HelloWorld
             Console.WriteLine("Luna is mine");
             Console.WriteLine("World on GitHub");
             Console.WriteLine("Earth on Local repository");
+            Console.WriteLine("Luna on GitHub");
         }
     }
 }
